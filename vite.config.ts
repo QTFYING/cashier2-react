5fc475537d064b5351f4c1159ffac5926f0f5aa9
Virgo <2811132560@qq.com>
2026-01-13 11:38:52 +0800

    添加submodules

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import { viteMockServe } from 'vite-plugin-mock';
@@ -13,15 +14,11 @@ export default defineConfig({
       enable: true,
     }),
   ],
-
-  optimizeDeps: {
-    // 告诉 Vite 不要预构建这些包，把它们当做源码处理
-    exclude: ['@my-cashier/core', '@my-cashier/types'],
-  },
-  server: {
-    fs: {
-      // 允许 Vite 访问项目根目录之外的文件 (因为 link 到了 ../../github)
-      allow: ['../..'],
+  resolve: {
+    alias: {
+      '@my-cashier/core': path.resolve(__dirname, 'libs/cashier/packages/core/src/index.ts'),
+      '@my-cashier/types': path.resolve(__dirname, 'libs/cashier/packages/types/src/index.ts'),
+      '@my-cashier/utils': path.resolve(__dirname, 'libs/cashier/packages/utils/src/index.ts'),
     },
   },
 });
